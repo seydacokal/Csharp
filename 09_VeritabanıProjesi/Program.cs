49ef0b78b027e7e910e584955b1669af13b2af37
Şeyda Çokal <seydacokal502@gmail.com>
2025-03-05 13:30:28 +0300

    Veritabanı Projesi tamamlandı

--- 09_VeritabanıProjesi/Program.cs
+++ 09_VeritabanıProjesi/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,43 @@ namespace _09_VeritabanıProjesi
     {
         static void Main(string[] args)
         {
+            //Ado.net
+
+            Console.WriteLine("***** C# Veri Tabanlı Ürün Kategori Bilgi Sistemi ***** ");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNumber;
+
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("1- Kategoriler");
+            Console.WriteLine("2- Ürünler");
+            Console.WriteLine("3- Siparişler");
+            Console.WriteLine("4- Çıkış Yap");
+            Console.Write("Lütfen Getirmek İstediğiniz Tablo Numarasını Giriniz: ");
+            tableNumber = Console.ReadLine();
+            Console.WriteLine("-------------------------------------------------");
+
+            SqlConnection connection = new SqlConnection("Data Source = localhost;initial " +
+                "Catalog=EğitimKampiDb;integrated security = true");
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select * From TblCategory", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);       
+            connection.Close();
+
+            foreach(DataRow row in dataTable.Rows)
+            {
+                foreach(var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString());
+                }
+                Console.WriteLine();
+            }
+
+            Console.Read();
+
         }
     }
 }
